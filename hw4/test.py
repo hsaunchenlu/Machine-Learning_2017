b35234bc6f98c94ff64aa936bdea8390505f4550
hsaunchenlu <r04522631@ntu.edu.tw>
2017-05-14 16:58:52 +0800

    test

--- hw4/test.py
+++ hw4/test.py
@@ -7,6 +7,11 @@ import numpy as np
 from sklearn.svm import LinearSVR as SVR
 from sklearn.neighbors import NearestNeighbors
 from sklearn.externals import joblib
+import sys
+
+inputpath = sys.argv[1]
+outputpath = sys.argv[2]
+
 
 def get_eigenvalues(data):
     SAMPLE = 300 # sample some points to estimate
@@ -28,7 +33,7 @@ def get_eigenvalues(data):
 
 # predict
 model = joblib.load('model.sav')
-testdata = np.load('data.npz')
+testdata = np.load(inputpath)
 
 
 
@@ -42,7 +47,7 @@ for i in range(200):
 test_X = np.array(test_X)
 pred_y = model.predict(test_X)
 
-with open('ans.csv', 'w') as f:
+with open(outputpath, 'w') as f:
     print('SetId,LogDim', file=f)
     for i, d in enumerate(pred_y):
         string = str(i) + ',' + str(np.log(d)) + '\n'
