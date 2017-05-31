9ad558c2e783396490e9a23e022ef684e3079524
hsaunchenlu <r04522631@ntu.edu.tw>
2017-05-31 14:49:14 +0800

    debug

--- hw6/hw6_best.py
+++ hw6/hw6_best.py
@@ -1,74 +1,94 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[ ]:
 
-#Import packages
+import math
 import pandas as pd
+from keras.callbacks import Callback, EarlyStopping, ModelCheckpoint
 import numpy as np
-from keras.layers import Embedding, Reshape, Merge, Dropout, Dense
-from keras.models import Sequential
+from itertools import product
+from keras import backend as K
+from keras.layers import Input, Embedding, Flatten, Dense, Dropout
+from keras.layers.merge import add, dot, concatenate
+from keras.models import Model
+from keras.optimizers import Adam
 import sys
 
-class CFModel(Sequential):
+def MF(n_U,n_M,F):    
+    U_input = Input(shape=(1,), dtype='int64', name='users')
+    M_input = Input(shape=(1,), dtype='int64', name='movies')
 
-    def __init__(self, n_users, m_items, k_factors, **kwargs):
-        P = Sequential()
-        P.add(Embedding(n_users, k_factors, input_length=1))
-        P.add(Reshape((k_factors,)))
-        Q = Sequential()
-        Q.add(Embedding(m_items, k_factors, input_length=1))
-        Q.add(Reshape((k_factors,)))
-        super(CFModel, self).__init__(**kwargs)
-        self.add(Merge([P, Q], mode='dot', dot_axes=1))
+    U_embedding = Embedding(input_dim=max_userid, output_dim=F)(U_input)
+    M_embedding = Embedding(input_dim=max_movieid, output_dim=F)(M_input)
 
-    def rate(self, user_id, item_id):
-        return self.predict([np.array([user_id]), np.array([item_id])])[0][0]
+    predicted_preference = dot(inputs=[U_embedding, M_embedding], axes=2)
+    predicted_preference = Flatten()(predicted_preference)
+    
+    model = Model(inputs=[U_input, M_input],outputs=predicted_preference)
+    return model
 
-class DeepModel(Sequential):
+def IMF(n_U,n_M,F):    
+    U_input = Input(shape=(1,), dtype='int64', name='users')
+    M_input = Input(shape=(1,), dtype='int64', name='movies')
 
-    def __init__(self, n_users, m_items, k_factors, p_dropout=0.1, **kwargs):
-        P = Sequential()
-        P.add(Embedding(n_users, k_factors, input_length=1))
-        P.add(Reshape((k_factors,)))
-        Q = Sequential()
-        Q.add(Embedding(m_items, k_factors, input_length=1))
-        Q.add(Reshape((k_factors,)))
-        super(DeepModel, self).__init__(**kwargs)
-        self.add(Merge([P, Q], mode='concat'))
-        self.add(Dropout(p_dropout))
-        self.add(Dense(k_factors, activation='relu'))
-        self.add(Dropout(p_dropout))
-        self.add(Dense(1, activation='linear'))
+    U_embedding = Embedding(input_dim=max_userid, output_dim=F)(U_input)
+    M_embedding = Embedding(input_dim=max_movieid, output_dim=F)(M_input)
 
-    def rate(self, user_id, item_id):
-        return self.predict([np.array([user_id]), np.array([item_id])])[0][0]
+    predicted_preference = dot(inputs=[U_embedding, M_embedding], axes=2)
+
+    U_bias = Embedding(input_dim=max_userid, output_dim=1, name='user_bias', input_length=1)(U_input)
+    M_bias = Embedding(input_dim=max_movieid, output_dim=1, name='movie_bias', input_length=1)(M_input)
+
+    predicted_preference = add(inputs=[predicted_preference, M_bias, U_bias])
+    predicted_preference = Flatten()(predicted_preference)
+    
+    model = Model(inputs=[U_input, M_input],outputs=predicted_preference)
+    return model
 
 
-# In[2]:
+def DeepMF(n_U,n_M,F):    
+    U_input = Input(shape=(1,), dtype='int64', name='users')
+    M_input = Input(shape=(1,), dtype='int64', name='movies')
+
+    U_embedding = Embedding(input_dim=max_userid, output_dim=F)(U_input)
+    M_embedding = Embedding(input_dim=max_movieid, output_dim=F)(M_input)
+    
+    #U_bias = Embedding(input_dim=max_userid, output_dim=1, name='user_bias', input_length=1)(U_input)
+    #M_bias = Embedding(input_dim=max_movieid, output_dim=1, name='movie_bias', input_length=1)(M_input)
+
+    concatenation = concatenate(inputs=[U_embedding, M_embedding])
+
+    dropout = Dropout(.1)(concatenation)
+    dense_layer = Dense(activation='relu', units=120)( dropout)
+    dropout = Dropout(.1)(dense_layer)
+    
+    predicted_preference = Dense(activation='linear',units=1, name='predicted_preference')(dropout)
+    predicted_preference = Flatten()(predicted_preference)
+    
+    #predicted_preference = add(inputs=[predicted_preference, M_bias, U_bias])
+    #predicted_preference = Flatten()(predicted_preference)
+    
+    model = Model(inputs=[U_input, M_input],outputs=predicted_preference)
+    return model
 
 #Define constants
-TESTING_CSV_FILE = sys.argv[1]
+TESTING_CSV_DIR = sys.argv[1]
 OUTPUT_PATH = sys.argv[2]
-MODEL_WEIGHTS_FILE = 'deepweights.h5'
+MODEL_WEIGHTS_FILE = 'DeepMF120.h5'
 K_FACTORS = 120
-
-
 max_userid = 6040
 max_movieid  = 3952
-# In[1]:
 
 #Import data
 
 
-test = pd.read_csv(TESTING_CSV_FILE, usecols=['TestDataID', 'UserID', 'MovieID'])
+test = pd.read_csv(TESTING_CSV_DIR+'train.csv', usecols=['TestDataID', 'UserID', 'MovieID'])
 print (len(test), 'descriptions of', max_movieid, 'movies loaded.')
 
 
-# In[11]:
-
 #Predict
-trained_model = DeepModel(max_userid, max_movieid, K_FACTORS)
+trained_model = DeepMF(max_userid, max_movieid, K_FACTORS)
 
 trained_model.load_weights(MODEL_WEIGHTS_FILE)
 
@@ -81,9 +101,4 @@ with open(OUTPUT_PATH,'w') as sm:
     print('\"TestDataID\",\"Rating\"',file=sm)
     for i in range(len(ids )):
         print('\"%d\",\"%.1f\"'%(ids[i],prediction[i]),file=sm)
-
-
-# In[ ]:
-
-
 
